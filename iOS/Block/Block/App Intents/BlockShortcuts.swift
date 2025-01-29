ffa6cb5609746e342a6abeaac970a8157b564074
Jeffrey Yao <33971845+jeffreydyao@users.noreply.github.com>
2025-01-29 23:06:13 +1100

    refactor: show app intents in spotlight, invoke dialog when returning session duration

--- iOS/Block/Block/App Intents/BlockShortcuts.swift
+++ iOS/Block/Block/App Intents/BlockShortcuts.swift
@@ -5,43 +5,55 @@
 //  Created by Jeffrey Yao on 21/1/2025.
 //
 
-import Foundation
 import AppIntents
+import Foundation
 
 class BlockShortcuts: AppShortcutsProvider {
-    static let shortcutTileColor: ShortcutTileColor = ShortcutTileColor.lightBlue
-    
+    static let shortcutTileColor: ShortcutTileColor = ShortcutTileColor
+        .lightBlue
+
     static var appShortcuts: [AppShortcut] {
-        AppShortcut(intent: StartManualSession(), phrases: [
-            "Start a session", // Starting a Block session directly is the default if invoked via voice or Spotlight. We don't block the user on tapping a Block.
-            "Start a block session",
-            "Start a manual block session",
-        ],
-                    shortTitle: "Start Session",
-                    systemImageName: "play.fill"
+        AppShortcut(
+            intent: StartManualSession(),
+            phrases: [
+                "Start a session with \(.applicationName)",  // Starting a Block session directly is the default if invoked via voice or Spotlight. We don't block the user on tapping a Block.
+                "Start a \(.applicationName) session",
+                "Start a session using \(.applicationName)",
+                "Start a manual \(.applicationName) session",
+            ],
+            shortTitle: "Start Session",
+            systemImageName: "play.fill"
         )
-        AppShortcut(intent: StartNFCSession(), phrases: [
-            "Start a session using NFC",
-            "Start a NFC session",
-            "Start a NFC block session"
-        ],
-                    shortTitle: "Start NFC Session",
-                    systemImageName: "play.fill"
+        AppShortcut(
+            intent: StartNFCSession(),
+            phrases: [
+                "Start a \(.applicationName) session using NFC",
+                "Start a NFC session using  \(.applicationName)",
+                "Start a NFC \(.applicationName) session",
+            ],
+            shortTitle: "Start NFC Session",
+            systemImageName: "play.fill"
         )
-        AppShortcut(intent: EndSession(), phrases: [
-            "End a session",
-            "End a block session"
-        ],
-                    shortTitle: "End Session",
-                    systemImageName: "stop.fill"
+        AppShortcut(
+            intent: EndSession(),
+            phrases: [
+                "End a session",
+                "End the \(.applicationName) session",
+                "End a \(.applicationName) session",
+                "End the session using \(.applicationName)",
+            ],
+            shortTitle: "End Session",
+            systemImageName: "stop.fill"
         )
-        AppShortcut(intent: GetSessionDuration(), phrases: [
-            "Get session duration",
-            "Get block session duration",
-            "How long has my block session been active for?",
-        ],
-                    shortTitle: "Get Session Duration",
-                    systemImageName: "stopwatch.fill"
+        AppShortcut(
+            intent: GetSessionDuration(),
+            phrases: [
+                "Get session duration",
+                "Get \(.applicationName) session duration",
+                "How long has my \(.applicationName) session been active for?",
+            ],
+            shortTitle: "Get Session Duration",
+            systemImageName: "stopwatch.fill"
         )
     }
 }
--- iOS/Block/Block/App Intents/GetSessionDuration.swift
+++ iOS/Block/Block/App Intents/GetSessionDuration.swift
@@ -13,11 +13,20 @@ struct GetSessionDuration: AppIntent {
     static let description = IntentDescription("Provides the seconds elapsed for the current session, if any.")
     
     @MainActor
-    func perform() async throws -> some IntentResult & ReturnsValue<TimeInterval> {
+    func perform() async throws -> some IntentResult & ProvidesDialog & ReturnsValue<TimeInterval> {
         let now = Date()
         let start = session.startDate ?? now
         let duration = now.timeIntervalSince(start)
-        return .result(value: duration)
+
+        let formatter = DateComponentsFormatter()
+        formatter.allowedUnits = [.day, .hour, .minute, .second]
+        formatter.unitsStyle = .full
+        formatter.maximumUnitCount = 2
+        formatter.includesApproximationPhrase = false
+        formatter.includesTimeRemainingPhrase = false
+        let formattedDuration = formatter.string(from: duration) ?? "\(Int(round(duration))) seconds"
+        
+        return .result(value: duration, dialog: "Block session active for \(formattedDuration).")
     }
     
     @Dependency
